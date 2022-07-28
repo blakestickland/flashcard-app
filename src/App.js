95bfed42f44dd80190be69be761f7b477315a252
Blake Stickland <blakelists@yahoo.com.au>
2022-07-28 16:38:07 +1000

    feature/card grid reacts to window size changes

--- src/App.js
+++ src/App.js
@@ -35,7 +35,9 @@ function App() {
   }
 
   return (
-    <FlashcardList flashcards={flashcards} />
+    <div className='container'>
+      <FlashcardList flashcards={flashcards} />
+    </div>
   )
 }
 
